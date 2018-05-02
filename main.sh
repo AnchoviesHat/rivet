fd5556022142dd99854f7470a923c1f1819a5b69
Colin Davidson <colrdavidson@gmail.com>
2018-05-01 20:07:02 -0400

    Add instruction counter, load and store

--- main.sh
+++ main.sh
@@ -1,8 +1,9 @@
 #!/bin/sh --posix
 
-rg0=5
-rg1=4
-rg2=4
+pc=1
+rg0=0
+rg1=0
+rg2=0
 
 get_register()
 {
@@ -22,7 +23,8 @@ get_register()
             return $rg2
             ;;
         *)
-            return -1
+            echo Invalid register $1
+            exit 1
             ;;
     esac
 }
@@ -43,37 +45,98 @@ set_register()
             rg2=$value
             ;;
         *)
-            return -1
+            echo Invalid register $1
+            exit 1
             ;;
     esac
 
     return 0
 }
 
-inst="sub r1 r2 r3"
-IFS=' '
-set -- $inst
-op=$1
-reg_1=$2
-reg_2=$3
-ret=$4
+echo '
 
-case $op in
-    add)
-        a=$(get_register $reg_1)
-        b=$(get_register $reg_2)
-        set_register $ret $(($a + $b))
-        v=$(get_register $ret)
-        echo $a + $b = $v
-        ;;
-    sub)
-        a=$(get_register $reg_1)
-        b=$(get_register $reg_2)
-        set_register $ret $(($a - $b))
-        v=$(get_register $ret)
-        echo $a - $b = $v
-        ;;
-    *)
-        echo invalid op $op
-        ;;
-esac
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+' > mem
+
+prog_size=$(wc -l < insts)
+
+while [ $pc != $prog_size ];
+do
+    inst=$(sed -n "$pc"p insts)
+
+    IFS=' '
+    set -- $inst
+    op=$1
+    reg_1=$2
+    reg_2=$3
+    imm=$3
+    ret=$4
+
+    case $op in
+        add)
+            a=$(get_register $reg_1)
+            b=$(get_register $reg_2)
+            set_register $ret $(($a + $b))
+            v=$(get_register $ret)
+            echo $a + $b = $v
+            ;;
+        sub)
+            a=$(get_register $reg_1)
+            b=$(get_register $reg_2)
+            set_register $ret $(($a - $b))
+            v=$(get_register $ret)
+            echo $a - $b = $v
+            ;;
+        mul)
+            a=$(get_register $reg_1)
+            b=$(get_register $reg_2)
+            set_register $ret $(($a * $b))
+            v=$(get_register $ret)
+            echo $a \* $b = $v
+            ;;
+        div)
+            a=$(get_register $reg_1)
+            b=$(get_register $reg_2)
+            set_register $ret $(($a / $b))
+            v=$(get_register $ret)
+            echo $a / $b = $v
+            ;;
+        set)
+            set_register $reg_1 $imm
+            echo $reg_1 = $imm
+            ;;
+        load)
+            v=$(sed "$imm"!d mem)
+            set_register $reg_1 $v
+            echo $reg_1 = \(mem\["$imm"\]: $v\)
+            ;;
+        store)
+            a=$(get_register $reg_1)
+            sed -i '' "$imm"s/.*/$a/ mem
+            echo mem\["$imm"\] = \($reg_1: $a\)
+            ;;
+        *)
+            echo invalid op $op
+            ;;
+    esac
+
+    pc=$(($pc + 1))
+done
+
+rm mem
